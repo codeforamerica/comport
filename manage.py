5aac8fb926231bf5f08407454a353400e603b524
Chris Reade <creade@codeforamerica.org>
2015-09-01 16:48:49 -0700

    Initial Cookie Cutter Commit

---
+++ manage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from flask_script import Manager, Shell, Server
+from flask_migrate import MigrateCommand
+
+from comport.app import create_app
+from comport.user.models import User
+from comport.settings import DevConfig, ProdConfig
+from comport.database import db
+
+if os.environ.get("COMPORT_ENV") == 'prod':
+    app = create_app(ProdConfig)
+else:
+    app = create_app(DevConfig)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TEST_PATH = os.path.join(HERE, 'tests')
+
+manager = Manager(app)
+
+
+def _make_context():
+    """Return context dict for a shell session so you can access
+    app, db, and the User model by default.
+    """
+    return {'app': app, 'db': db, 'User': User}
+
+
+@manager.command
+def test():
+    """Run the tests."""
+    import pytest
+    exit_code = pytest.main([TEST_PATH, '--verbose'])
+    return exit_code
+
+
+manager.add_command('server', Server())
+manager.add_command('shell', Shell(make_context=_make_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
