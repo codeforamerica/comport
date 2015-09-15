49b9f31005543bb8a4e4033cbce6e3099a61d0b0
Chris Reade <creade@codeforamerica.org>
2015-09-15 10:58:48 -0700

    load data from test csv

--- comport/utils.py
+++ comport/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Helper utilities and decorators."""
 from flask import flash
+import string
+import random
 
 
 def flash_errors(form, category="warning"):
@@ -9,3 +11,6 @@ def flash_errors(form, category="warning"):
         for error in errors:
             flash("{0} - {1}"
                   .format(getattr(form, field).label.text, error), category)
+
+def random_string(N):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from flask_script import Manager, Shell, Server, prompt_bool
+from flask_script import Manager, Shell, Server, prompt_bool, prompt_pass
 from flask_migrate import MigrateCommand
 
 
@@ -10,6 +10,10 @@ from comport.user.models import User, Role
 from comport.department.models import Department, Extractor
 from comport.settings import DevConfig, ProdConfig
 from comport.database import db
+from comport.utils import random_string
+from comport.data.models import UseOfForceIncident
+
+import csv
 
 if os.environ.get("COMPORT_ENV") == 'prod':
     app = create_app(ProdConfig)
@@ -26,7 +30,7 @@ def _make_context():
     """Return context dict for a shell session so you can access
     app, db, and the User model by default.
     """
-    return {'app': app, 'db': db, 'User': User, 'Department': Department, 'Extractor': Extractor}
+    return {'app': app, 'db': db, 'User': User, 'Department': Department, 'Extractor': Extractor, 'UseOfForceIncident': UseOfForceIncident}
 
 
 @manager.command
@@ -39,7 +43,8 @@ def test():
 
 @manager.command
 def make_admin_user():
-    user = User.create(username="admin", email="email@example.com",password="password",active=True)
+    password = prompt_pass("Password")
+    user = User.create(username="admin", email="email@example.com",password=password,active=True)
     admin_role = Role(name='admin')
     admin_role.save()
     user.roles.append(admin_role)
@@ -47,9 +52,16 @@ def make_admin_user():
 
 @manager.command
 def make_normal_user():
-    department = Department.create(name="Busy Town Public Safety")
-    user = User.create(username="user", email="email2@example.com",password="password",active=True, department_id=department.id)
-    user.save()
+    department = Department.query.filter_by(name="Busy Town Public Safety").first()
+    if not department:
+        department = Department.create(name="Busy Town Public Safety")
+    if not User.query.filter_by(username="user").first():
+        User.create(username="user", email="email2@example.com",password="password",active=True, department_id=department.id)
+    with open('comport/testData/UOF.csv', 'rt') as f:
+        reader = csv.DictReader(f)
+        for incident in reader:
+            UseOfForceIncident.create(opaque_id=random_string(6), service_type=incident["SERVICE_TYPE"],department_id=department.id)
+
 
 @manager.command
 def delete_everything():
