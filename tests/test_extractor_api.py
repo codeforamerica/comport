95659a5fcea07a49519d5a9dddd73853b08a6064
Thomas Apodaca <tma@angrymonkey.com>
2016-03-24 23:50:45 -0700

    added tests for ois update, add

--- tests/test_extractor_api.py
+++ tests/test_extractor_api.py
@@ -396,3 +396,101 @@ class TestHeartbeat:
         assert check_ois.officer_age == sent_ois['officerAge']
         assert check_ois.officer_years_of_service == sent_ois['officerYearsOfService']
         assert check_ois.officer_condition == sent_ois['officerCondition']
+
+    def test_update_ois_data(self, testapp):
+        ''' Updated OIS data from the extractor is processed as expected.
+        '''
+        # Set up the extractor
+        department = Department.create(name="Good Police Department", short_name="GPD", load_defaults=False)
+        extractor, envs = Extractor.from_department_and_password(department=department, password="password")
+
+        # Set the correct authorization
+        testapp.authorization = ('Basic', (extractor.username, 'password'))
+
+        # Get a generated list of OIS descriptions from the JSON test client
+        test_client = JSONTestClient()
+        ois_data = test_client.get_prebaked_ois(last=1)
+        # post the json to the OIS URL
+        response = testapp.post_json("/data/OIS", params={'month': 0, 'year': 0, 'data': ois_data})
+
+        # assert that we got the expected reponse
+        assert response.status_code == 200
+        assert response.json_body['updated'] == 0
+        assert response.json_body['added'] == 1
+
+        # Get the second pre-baked ois incident
+        updated_ois_data = test_client.get_prebaked_ois(first=1, last=2)
+        # Swap in the opaque ID from the first ois incident
+        updated_ois_data[0]["opaqueId"] = ois_data[0]["opaqueId"]
+        # The ois incident won't be a match unless this field is the same
+        updated_ois_data[0]["officerIdentifier"] = ois_data[0]["officerIdentifier"]
+        # post the json to the ois URL
+        response = testapp.post_json("/data/OIS", params={'month': 0, 'year': 0, 'data': updated_ois_data})
+
+        # assert that we got the expected reponse
+        assert response.status_code == 200
+        assert response.json_body['updated'] == 1
+        assert response.json_body['added'] == 0
+
+        # There's only one complaint in the database.
+        all_ois = OfficerInvolvedShooting.query.all()
+        assert len(all_ois) == 1
+
+        # check the ois incident in the database against the updated data that was sent
+        sent_ois = updated_ois_data[0]
+        check_ois = OfficerInvolvedShooting.query.filter_by(opaque_id=sent_ois['opaqueId']).first()
+        assert check_ois.occured_date.strftime('%Y-%m-%d %-H:%-M:%S') == sent_ois['occuredDate']
+        assert check_ois.division == Cleaners.capitalize(sent_ois['division'])
+        assert check_ois.precinct == Cleaners.capitalize(sent_ois['precinct'])
+        assert check_ois.shift == Cleaners.capitalize(sent_ois['shift'])
+        assert check_ois.beat == Cleaners.capitalize(sent_ois['beat'])
+        assert check_ois.disposition == sent_ois['disposition']
+        assert check_ois.resident_race == Cleaners.race(sent_ois['residentRace'])
+        assert check_ois.resident_sex == Cleaners.sex(sent_ois['residentSex'])
+        assert check_ois.resident_age == sent_ois['residentAge']
+        assert check_ois.resident_weapon_used == Cleaners.resident_weapon_used(sent_ois['residentWeaponUsed'])
+        assert check_ois.resident_condition == sent_ois['residentCondition']
+        assert check_ois.officer_identifier == sent_ois['officerIdentifier']
+        assert check_ois.officer_weapon_used == sent_ois['officerForceType']
+        assert check_ois.officer_race == Cleaners.race(sent_ois['officerRace'])
+        assert check_ois.officer_sex == Cleaners.sex(sent_ois['officerSex'])
+        assert check_ois.officer_age == sent_ois['officerAge']
+        assert check_ois.officer_years_of_service == sent_ois['officerYearsOfService']
+        assert check_ois.officer_condition == sent_ois['officerCondition']
+
+    def test_post_ois_data_near_match_does_not_update(self, testapp):
+        ''' OIS data with the same ID but different details creates a new record.
+        '''
+        # Set up the extractor
+        department = Department.create(name="Good Police Department", short_name="GPD", load_defaults=False)
+        extractor, envs = Extractor.from_department_and_password(department=department, password="password")
+
+        # Set the correct authorization
+        testapp.authorization = ('Basic', (extractor.username, 'password'))
+
+        # Get a generated list of OIS descriptions from the JSON test client
+        test_client = JSONTestClient()
+        ois_data = test_client.get_prebaked_ois(last=1)
+        # post the json to the OIS URL
+        response = testapp.post_json("/data/OIS", params={'month': 0, 'year': 0, 'data': ois_data})
+
+        # assert that we got the expected reponse
+        assert response.status_code == 200
+        assert response.json_body['updated'] == 0
+        assert response.json_body['added'] == 1
+
+        # Get the second pre-baked ois incident
+        updated_ois_data = test_client.get_prebaked_ois(first=1, last=2)
+        # Swap in the opaque ID from the first ois incident
+        updated_ois_data[0]["opaqueId"] = ois_data[0]["opaqueId"]
+        # post the json to the ois URL
+        response = testapp.post_json("/data/OIS", params={'month': 0, 'year': 0, 'data': updated_ois_data})
+
+        # assert that we got the expected reponse
+        assert response.status_code == 200
+        assert response.json_body['updated'] == 0
+        assert response.json_body['added'] == 1
+
+        # There's only one complaint in the database.
+        all_ois = OfficerInvolvedShooting.query.all()
+        assert len(all_ois) == 2
