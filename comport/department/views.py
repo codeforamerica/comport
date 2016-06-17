05e291671d55f8eba62a56a046c569fddb603190
Edmund Korley <korley.em@gmail.com>
2016-06-17 16:43:35 -0700

    Assaults on officers schema page

--- comport/department/views.py
+++ comport/department/views.py
@@ -233,6 +233,14 @@ def use_of_force_schema(department_id):
         abort(404)
     return render_template("department/site/schema/complaints.html", department=department)
 
+@blueprint.route('/<int:department_id>/schema/assaultsonofficers')
+@login_required
+@admin_or_department_required()
+def assaults_on_officers_schema(department_id):
+    department = Department.get_by_id(department_id)
+    if not department:
+        abort(404)
+    return render_template("department/site/schema/assaults.html", department=department)
 
 # <<<<<<<< DATA ENDPOINTS >>>>>>>>>>
 @blueprint.route('/<int:department_id>/uof.csv')
