fd126b02d59143ecb0a868967d903e2d9a089010
Thomas Apodaca <tma@angrymonkey.com>
2016-08-30 15:47:34 -0700

    got rid of complaints-by-year default

--- tests/test_manage_methods.py
+++ tests/test_manage_methods.py
@@ -12,15 +12,15 @@ class TestManageMethods:
     def test_add_chart_blocks(self):
         ''' Can add default chart blocks to multiple departments.
         '''
-        department1 = Department.create(name="Spleen Police Department", short_name="SPD", load_defaults=False)
-        department2 = Department.create(name="Random Police Department", short_name="RPD", load_defaults=False)
+        department1 = Department.create(name="IM Police Department", short_name="IMPD", load_defaults=False)
+        department2 = Department.create(name="B Police Department", short_name="BPD", load_defaults=False)
 
         mock_chart_blocks = [
             ChartBlock(title="Open Data Introduction", slug="introduction", dataset="introduction"),
-            ChartBlock(title="Complaints By Year", slug="complaints-by-year", dataset="complaints"),
             ChartBlock(title="Complaints By Month", slug="complaints-by-month", dataset="complaints"),
             ChartBlock(title="Complaints By Allegation", slug="complaints-by-allegation", dataset="complaints"),
-            ChartBlock(title="Complaints By Allegation Type", slug="complaints-by-allegation-type", dataset="complaints")
+            ChartBlock(title="Complaints By Allegation Type", slug="complaints-by-allegation-type", dataset="complaints"),
+            ChartBlock(title="Complaints By Finding", slug="complaints-by-finding", dataset="complaints")
         ]
 
         with mock.patch.object(ChartBlockDefaults, 'defaults', mock_chart_blocks):
