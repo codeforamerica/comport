e9377a0d423d396ac1f514b474d509a11f1c3a4f
Thomas Apodaca <tma@angrymonkey.com>
2016-09-16 13:05:40 -0700

    updated factories got rid of unused DenominatorValueFactory trying out a LMPD uof incident factory

--- tests/factories.py
+++ tests/factories.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-from factory import Sequence, PostGenerationMethodCall
-from factory.fuzzy import FuzzyInteger
+import factory
+from factory.fuzzy import FuzzyText, FuzzyDate, FuzzyChoice
 from factory.alchemy import SQLAlchemyModelFactory
 
 from comport.user.models import User
 from comport.department.models import Department
-from comport.data.models import DenominatorValue
+from comport.data.models import UseOfForceIncidentLMPD
 from comport.database import db
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+import datetime
 
 class BaseFactory(SQLAlchemyModelFactory):
 
@@ -17,31 +16,53 @@ class BaseFactory(SQLAlchemyModelFactory):
         sqlalchemy_session = db.session
 
 class DepartmentFactory(BaseFactory):
-    name = Sequence(lambda n: "Department {0}".format(n))
-    short_name = Sequence(lambda n: "DPD{0}".format(n))
+    name = factory.Sequence(lambda n: "Factory{0} Police Department".format(n))
+    short_name = factory.Sequence(lambda n: "F{0}PD".format(n))
     load_defaults = False
 
     class Meta:
         model = Department
 
 class UserFactory(BaseFactory):
-    username = Sequence(lambda n: "user{0}".format(n))
-    email = Sequence(lambda n: "user{0}@example.com".format(n))
-    password = PostGenerationMethodCall('set_password', 'example')
+    username = factory.Sequence(lambda n: "user{0}".format(n))
+    email = factory.Sequence(lambda n: "user{0}@example.com".format(n))
+    password = factory.PostGenerationMethodCall('set_password', 'example')
     active = True
 
     class Meta:
         model = User
 
-class DenominatorValueFactory(BaseFactory):
-    month = Sequence(lambda n: datetime(2012, 1, 1) + relativedelta(months=n))
-    officers_out_on_service = FuzzyInteger(200, 500)
+class UseOfForceIncidentLMPDFactory(BaseFactory):
+    ''' Generate a fake LMPD uof incident. Must pass a department_id when using.
+    '''
+    opaque_id = FuzzyText(length=32)
+    occured_date = FuzzyDate(datetime.date(2015, 1, 1))
+    bureau = FuzzyText(length=16)
+    division = FuzzyText(length=16)
+    unit = FuzzyText(length=16)
+    platoon = FuzzyText(length=16)
+    disposition = FuzzyText(length=16)
+    use_of_force_reason = FuzzyText(length=16)
+    officer_force_type = FuzzyText(length=16)
+    service_type = FuzzyText(length=16)
+    arrest_made = FuzzyChoice([True, False])
+    arrest_charges = FuzzyText(length=16)
+    resident_injured = FuzzyChoice([True, False])
+    resident_hospitalized = FuzzyChoice([True, False])
+    resident_condition = FuzzyText(length=16)
+    officer_injured = FuzzyChoice([True, False])
+    officer_hospitalized = FuzzyChoice([True, False])
+    officer_condition = FuzzyText(length=16)
+    resident_identifier = FuzzyText(length=32)
+    resident_weapon_used = FuzzyText(length=16)
+    resident_race = FuzzyText(length=16)
+    resident_sex = FuzzyText(length=16)
+    resident_age = FuzzyText(length=16)
+    officer_race = FuzzyText(length=16)
+    officer_sex = FuzzyText(length=16)
+    officer_age = FuzzyText(length=16)
+    officer_years_of_service = FuzzyText(length=16)
+    officer_identifier = FuzzyText(length=32)
 
     class Meta:
-        model = DenominatorValue
-
-    @classmethod
-    def _after_postgeneration(cls, obj, create, results=None):
-        tmp = obj.month
-        obj.month = tmp.month
-        obj.year = tmp.year
+        model = UseOfForceIncidentLMPD
