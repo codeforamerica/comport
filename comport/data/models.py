60226a37b5d9ba51310128a4c47e49feaf9f2249
Thomas Apodaca <tma@angrymonkey.com>
2016-11-17 14:51:59 -0800

    remove bureau and division from BPD ois

--- comport/data/models.py
+++ comport/data/models.py
@@ -643,8 +643,6 @@ class OfficerInvolvedShootingBPD(SurrogatePK, Model):
     department_id = Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
     opaque_id = Column(db.String(255), unique=False, nullable=False)
     occured_date = Column(db.DateTime, nullable=True)
-    bureau = Column(db.String(255), unique=False, nullable=True)
-    division = Column(db.String(255), unique=False, nullable=True)
     assignment = Column(db.String(255), unique=False, nullable=True)
     has_disposition = Column(db.Boolean, nullable=True)
     resident_weapon_used = Column(db.String(255), unique=False, nullable=True)
@@ -669,7 +667,7 @@ class OfficerInvolvedShootingBPD(SurrogatePK, Model):
     def get_csv_schema(cls):
         ''' Return the CSV column headers and variable names, and the variable names expected from the extractor.
         '''
-        return [("id", "opaque_id", "opaqueId"), ("occurredDate", "occured_date", "occuredDate"), ("bureau", "bureau", "bureau"), ("division", "division", "division"), ("assignment", "assignment", "assignment"), ("hasDisposition", "has_disposition", "hasDisposition"), ("residentWeaponUsed", "resident_weapon_used", "residentWeaponUsed"), ("officerWeaponUsed", "officer_weapon_used", "officerWeaponUsed"), ("serviceType", "service_type", "serviceType"), ("residentCondition", "resident_condition", "residentCondition"), ("officerCondition", "officer_condition", "officerCondition"), ("residentIdentifier", "resident_identifier", "residentIdentifier"), ("residentRace", "resident_race", "residentRace"), ("residentSex", "resident_sex", "residentSex"), ("residentAge", "resident_age", "residentAge"), ("officerRace", "officer_race", "officerRace"), ("officerSex", "officer_sex", "officerSex"), ("officerAge", "officer_age", "officerAge"), ("officerYearsOfService", "officer_years_of_service", "officerYearsOfService"), ("officerIdentifier", "officer_identifier", "officerIdentifier")]
+        return [("id", "opaque_id", "opaqueId"), ("occurredDate", "occured_date", "occuredDate"), ("assignment", "assignment", "assignment"), ("hasDisposition", "has_disposition", "hasDisposition"), ("residentWeaponUsed", "resident_weapon_used", "residentWeaponUsed"), ("officerWeaponUsed", "officer_weapon_used", "officerWeaponUsed"), ("serviceType", "service_type", "serviceType"), ("residentCondition", "resident_condition", "residentCondition"), ("officerCondition", "officer_condition", "officerCondition"), ("residentIdentifier", "resident_identifier", "residentIdentifier"), ("residentRace", "resident_race", "residentRace"), ("residentSex", "resident_sex", "residentSex"), ("residentAge", "resident_age", "residentAge"), ("officerRace", "officer_race", "officerRace"), ("officerSex", "officer_sex", "officerSex"), ("officerAge", "officer_age", "officerAge"), ("officerYearsOfService", "officer_years_of_service", "officerYearsOfService"), ("officerIdentifier", "officer_identifier", "officerIdentifier")]
 
     @classmethod
     def add_or_update_incident(cls, department, incident):
@@ -677,9 +675,7 @@ class OfficerInvolvedShootingBPD(SurrogatePK, Model):
         '''
         # get a cleaner instance
         cleaner = Cleaners()
-        # capitalize the location
-        incident["bureau"] = cleaner.capitalize(incident["bureau"])
-        incident["division"] = cleaner.capitalize(incident["division"])
+        # capitalize the assignment
         incident["assignment"] = cleaner.capitalize(incident["assignment"])
         # clean weapon, race, gender
         incident["residentWeaponUsed"] = cleaner.resident_weapon_used(incident["residentWeaponUsed"])
@@ -708,8 +704,6 @@ class OfficerInvolvedShootingBPD(SurrogatePK, Model):
             department_id=department.id,
             opaque_id=incident["opaqueId"],
             occured_date=parse_date(incident["occuredDate"]),
-            bureau=incident["bureau"],
-            division=incident["division"],
             assignment=incident["assignment"],
             has_disposition=incident["hasDisposition"],
             resident_weapon_used=incident["residentWeaponUsed"],
