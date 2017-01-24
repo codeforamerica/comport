110644af89a25f7c1df2cbfa0be832097b4b278b
Thomas Apodaca <tma@angrymonkey.com>
2017-01-23 16:15:00 -0800

    first pass at single submission edit form

--- comport/content/views.py
+++ comport/content/views.py
@@ -8,6 +8,79 @@ from comport.content.models import ChartBlock
 blueprint = Blueprint("content", __name__, url_prefix='/content',
                       static_folder="../static")
 
+@blueprint.route("/<int:department_id>", methods=["POST"])
+@login_required
+def edit_page(department_id):
+    if not current_user.has_department(department_id) and not current_user.is_admin():
+        abort(401)
+
+    # Importing this at the top of file caused a circular dependency
+    # issue so we do a delayed import here
+    from comport.department.models import Department
+
+    # let's sort the contents of the form submission
+    changes = {}
+    for formfield in request.form:
+        # formfield = 'chart_order-pursuits-schema-field-dui-arrest'
+        value_name, slug = formfield.split("-", 1)
+        if slug not in changes:
+            changes[slug] = {}
+
+        changes[slug][value_name] = request.form[formfield]
+
+    for chart_slug in changes:
+        block = ChartBlock.query.filter_by(department_id=department_id, slug=chart_slug).first()
+
+        if not block:
+            abort(404)
+
+        # set values if they were passed
+        block.title = changes[chart_slug]["chart_title"] if "chart_title" in changes[chart_slug] and changes[chart_slug]["chart_title"] else block.title
+        block.content = changes[chart_slug]["chart_content"] if "chart_content" in changes[chart_slug] and changes[chart_slug]["chart_content"] else block.content
+
+        if ("chart_order" in changes[chart_slug] and changes[chart_slug]["chart_order"]):
+                try:
+                    int(changes[chart_slug]["chart_order"])
+                except:
+                    pass
+                else:
+                    block.order = changes[chart_slug]["chart_order"]
+
+        block.save()
+
+        if "blocks_prefix" in changes[chart_slug]:
+            department = Department.query.filter_by(id=department_id).first()
+            blocks = department.get_blocks_by_slug_startswith(changes[chart_slug]["blocks_prefix"])
+
+            block.order = max(min(block.order, len(blocks) - 1), 0)
+            block.save()
+
+            # Init new array to length of blocks
+            new_blocks = [None] * len(blocks)
+
+            # Put block of interest where it's supposed to be
+            new_blocks[block.order] = block
+            blocks.pop(blocks.index(block))
+
+            # Iterate through new_blocks
+            for index, value in enumerate(new_blocks):
+                if value is not None:
+                    continue
+
+                move_block = blocks.pop(0)
+                move_block.order = index
+                move_block.save()
+                new_blocks[index] = move_block
+
+    if request.referrer and 'edit' in request.referrer:
+        new_path = urlparse(request.referrer.replace('/edit/', '/preview/')).path
+    else:
+        new_path = url_for(
+            'department.department_dashboard', department_id=department_id
+        )
+
+    return redirect(new_path)
+
 @blueprint.route("/<string:chart_slug>/<int:department_id>", methods=["POST"])
 @login_required
 def edit_chart_block(department_id, chart_slug):
